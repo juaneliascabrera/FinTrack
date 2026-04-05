80b4cff6fd2f6e4e247973be4f42ab689b834968
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2026-04-05 12:55:49 -0300

    Added account can't change balance test.

--- fintrack-backend/tests/test_schemas.py
+++ fintrack-backend/tests/test_schemas.py
@@ -16,3 +16,17 @@ def test_can_not_change_user_email(session, default_user):
         service.update(1, updated_user)
 
     assert created_user.email == "test1@ejemplo.com"
+
+def test_can_not_change_acconut_balance(session, default_user, account_factory):    
+    user_service = UserService(session)
+    account_service = AccountService(session)
+    created_user = user_service.create(default_user)
+    new_account = account_factory(user_id = created_user.id, balance = 500)
+
+    created_account = account_service.create(new_account)
+    assert created_user.email == "test1@ejemplo.com"
+    with pytest.raises(ValidationError):
+        updated_user = UserUpdate(email = "test2@ejemplo.com")
+        user_service.update(1, updated_user)
+
+    assert created_user.email == "test1@ejemplo.com"

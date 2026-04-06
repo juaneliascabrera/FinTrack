3d5b0af21d03da90794945a3770dad62e415e445
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2026-04-06 17:55:46 -0300

    Added a first model for Transaction.

--- fintrack-backend/app/models.py
+++ fintrack-backend/app/models.py
@@ -1,12 +1,18 @@
+from sqlalchemy.orm import foreign
+from datetime import datetime, timezone
 from typing import List
 
 from sqlmodel import Field, Relationship, SQLModel
 
+class TransactionType(Enum):
+    INCOME = "income"
+    EXPENSE = "expense"
+    TRANSFER = "transfer"    
 
 class Account(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
-    balance: int
+    balance: float
     user: "User" = Relationship(back_populates="accounts")
     user_id: int = Field(foreign_key="user.id", index=True)
 
@@ -17,3 +23,14 @@ class User(SQLModel, table=True):
     email: str = Field(unique=True, index=True)
     password: str
     accounts: List[Account] = Relationship(back_populates="user")
+
+class Transaction(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    amount: float
+    description: str | None = None
+    category: str | None = None
+    timestamp: datetime = Field(default_factory=datetime.now(timezone.utc))
+    from_account_id: int = Field(foreign_key="account.id", index=True)
+    to_account_id: int | None = Field(default=None, foreign_key="account.id", index=True)
+    type: TransactionType
+     

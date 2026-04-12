81f795f7f2aac342debdbf35f116baa8f1151201
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2026-04-12 15:10:22 -0300

    Added isLogging state to avoid double submits.2

--- fintrack-frontend/src/pages/Login.tsx
+++ fintrack-frontend/src/pages/Login.tsx
@@ -5,19 +5,25 @@ export default function Login() {
     // First we'll define the state for both email and password.
     const [formData, setFormData] = useState<LoginRequest>({ username: '', password: '' });
     const [error, setError] = useState("");
-
+    // We need an isLogging state to avoid double submits.
+    const [isLoggin, setIsLogging] = useState(false);
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     }
 
     const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+        setIsLogging(true);
+        console.log("Logging in...")
         e.preventDefault();
         try {
             const data = await login(formData);
             localStorage.setItem('token', data.access_token);
         }
         catch (err: any) {
-            setError(err.response?.data?.detail || "Error de conexión");
+            setError(err.response?.data?.detail || "Unknown error");
+        }
+        finally {
+            setIsLogging(true);
         }
     };
     return (

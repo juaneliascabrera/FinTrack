fee84c6a820c375de7ac1aa1427e902851eebe7f
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2026-04-12 13:50:34 -0300

    Working login.

--- fintrack-frontend/src/pages/Login.tsx
+++ fintrack-frontend/src/pages/Login.tsx
@@ -1,14 +1,39 @@
-import { login } from '../services/auth';
-
+import { login, type LoginRequest } from '../services/auth';
+import { useState } from 'react';
+import api from '../services/api'
 export default function Login() {
-    const handleSubmit = (e: React.SubmitEvent) => {
+    // First we'll define the state for both email and password.
+    const [formData, setFormData] = useState<LoginRequest>({ username: '', password: '' });
+    const [error, setError] = useState("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log("Submit");
+        try {
+            const data = await login(formData);
+            localStorage.setItem('token', data.access_token);
+        }
+        catch (err: any) {
+            setError(err.response?.data?.detail || "Error de conexión");
+        }
     };
     return (
         <form onSubmit={handleSubmit}>
-            <input type="email" />
-            <input type="password" />
+            <input
+                type="email"
+                placeholder='Email'
+                onChange={handleChange}
+                name='username'
+            />
+            <input
+                type="password"
+                placeholder='Password'
+                name='password'
+                onChange={handleChange} />
+
             <button type="submit">Entrar</button>
         </form>
     );
--- fintrack-frontend/src/services/auth.ts
+++ fintrack-frontend/src/services/auth.ts
@@ -1,6 +1,19 @@
 import api from './api'
 
-export async function login(data) {
-    const response = await api.post("/login", data);
+export interface LoginRequest {
+    username: string;
+    password: string;
+}
+
+export interface AuthResponse {
+    access_token: string;
+    token_type: string;
+}
+
+export async function login(data: LoginRequest) {
+    const formData = new URLSearchParams();
+    formData.append('username', data.username);
+    formData.append('password', data.password);
+    const response = await api.post("/auth/login", formData);
     return response.data;
 }

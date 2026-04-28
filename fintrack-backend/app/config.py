bde0ce32ce78d6b581b0027ed0dff46ab7ac2e7c
Juan Elias Cabrera <jecabrera@dc.uba.ar>
2026-04-28 19:06:14 -0300

    Remove .env file dependency, use system env vars only

--- fintrack-backend/app/config.py
+++ fintrack-backend/app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: float
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(extra="ignore")
 
 
 settings = Settings()
